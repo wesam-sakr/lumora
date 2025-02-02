8b8df7625aa994f7889ba000a97b6fb684495278
wesam-sakr <wesamsakr801@gmail.com>
2025-02-02 16:42:40 +0200

    lumora

--- js/script.js
+++ js/script.js
@@ -13,194 +13,82 @@ if (bodyDir == "rtl") {
   dirAr = false;
 }
 $(document).ready(function () {
-  
-  $('#lang').on('click', function() {
+
+  $('#lang').on('click', function () {
     $('html').attr('dir', $('html').attr('dir') === 'ltr' ? 'rtl' : 'ltr');
     $('html').attr('lang', $('html').attr('lang') === 'en' ? 'ar' : 'en');
-    $('#lang-text').text($('html').attr('lang') === 'en' ? 'العربية' : 'English' );
+    $('#lang-text').text($('html').attr('lang') === 'en' ? 'العربية' : 'English');
     $('.owl-carousel').toggleClass('owl-rtl');
     $('#bootstrap-style').attr('href', $('html').attr('lang') === 'en' ? 'css/bootstrap.min.css' : 'css/bootstrap.rtl.min.css');
   });
+
+  $(".file-input").change(function () {
+    const fileInput = $(this).find('[type="file"]')[0];
+    const label = $(this).find("[data-js-label]")[0];
+  
+    if (!fileInput.files.length) return;
+  
+    // الحصول على أسماء جميع الملفات المختارة
+    const fileNames = Array.from(fileInput.files).map(file => file.name).join(", ");
+  
+    $(label).html(fileNames);
+  });
+  
 
   // Scroll to the top of the page
   window.addEventListener('scroll', () => {
     document.getElementById('scrollUp').style.display = window.scrollY > 300 ? 'block' : 'none';
   });
 
-  // change input by purpose
-  $('input[name="purpose"]').change(function () {
-    if ($(this).attr("id") == "land") {
-      $(".project").hide();
-    } else $(".project").show();
-  });
-
-  // counter
-  window.addEventListener("scroll", function () {
-    if ($(".counters").length > 0) {
-      var counters = document.querySelectorAll(".counter");
-      var counterStart = counters[0].offsetTop - 500;
-      var speed = 200;
-      if (this.document.documentElement.scrollTop > counterStart) {
-        counters.forEach((counter) => {
-          var upTo = function () {
-            var target = Number(counter.getAttribute("data-target"));
-            var count = Number(counter.innerText);
-            var inc = Math.ceil(target / speed);
-            setTimeout(upTo, 300);
-
-            if (count < target) {
-              counter.innerText = '+' + count + inc;
-            } else counter.innerText = '+' + target;
-          };
-          upTo();
-        });
-      }
-    }
-  });
-
-  // toggle fav
-  $(".fav").click(function () {
-    $(this).find("i").toggleClass("fa-solid fa-regular");
-  });
-
-  // toggle password type
-  $('.pass').click(function () {
-    $(this).children('i').toggleClass("bi-unlock bi-lock");
-    var pass = $(this).closest('.input-group').find('input')[0];
-    console.log(pass);
-    if (pass.type == "password") {
-      pass.setAttribute("type", "text");
-    } else {
-      pass.setAttribute("type", "password");
-    }
-  })
-
-
-  // عند الضغط على زر التبديل، نحدد العنصر المستهدف ونبدل الكلاس "show"
-  $("[data-toggle]").click(function () {
-    let target = $(this).data("toggle");
-    $(target).toggleClass("show");
-  });
-
-  // عند الضغط على زر الإغلاق، نحذف الكلاس "show" من العنصر المستهدف
-  $("[data-close]").click(function () {
-    let target = $(this).data("close");
-    $(target).removeClass("show");
-  });
-
-  // verification code OTP
-  if ($('#verification-input').length > 0) {
-    const inputs = Array.from(document.getElementById("verification-input").children);
-    function getFirstEmptyIndex() {
-      return inputs.findIndex((input) => input.value === "");
-    }
-    inputs.forEach((input, i) => {
-      input.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace") {
-          if (input.value === "" && i > 0) {
-            inputs[i - 1].value = "";
-            inputs[i - 1].focus();
-          }
-
-          for (let j = i; j < inputs.length; j++) {
-            let value = inputs[j + 1] ? inputs[j + 1].value : "";
-            inputs[j].setRangeText(value, 0, 1, "start");
-          }
-        }
-
-        if (e.key === "ArrowLeft" && i > 0) {
-          inputs[i - 1].focus();
-        }
-
-        if (e.key === "ArrowRight" && i < inputs.length - 1) {
-          inputs[i + 1].focus();
-        }
-      });
-
-      input.addEventListener("input", (e) => {
-        input.value = "";
-
-        const start = getFirstEmptyIndex();
-        inputs[start].value = e.data;
-
-        if (start + 1 < inputs.length) inputs[start + 1].focus();
-      });
-
-      input.addEventListener("paste", (e) => {
-        e.preventDefault();
-
-        const text = (event.clipboardData || window.clipboardData).getData("text");
-        const firstEmpty = getFirstEmptyIndex();
-        const start = firstEmpty !== -1 ? Math.min(i, firstEmpty) : i;
-
-        for (let i = 0; start + i < inputs.length && i < text.length; i++) {
-          inputs[start + i].value = text.charAt(i);
-        }
-
-        inputs[Math.min(start + text.length, inputs.length - 1)].focus();
-      });
-
-      input.addEventListener("focus", () => {
-        const start = getFirstEmptyIndex();
-        if (start !== -1 && i > start) inputs[start].focus();
-      });
-    });
-  }
-
-  // upload profile pic 
-  if ($(".profile-pic").length > 0) {
-    const imgDiv = document.querySelector(".profile-pic");
-    const img = document.querySelector("#photo");
-    const file = document.querySelector("#file");
-    const uploadBtn = document.querySelector("#uploadBtn");
-
-    //if user hover on img div
-
-    imgDiv.addEventListener("mouseenter", function () {
-      uploadBtn.style.display = "block";
-    });
-
-    //if we hover out from img div
-
-    imgDiv.addEventListener("mouseleave", function () {
-      uploadBtn.style.display = "none";
-    });
-
-    //when we choose a pic to upload
-
-    file.addEventListener("change", function () {
-      const choosedFile = this.files[0];
-      if (choosedFile) {
-        const reader = new FileReader();
-        reader.addEventListener("load", function () {
-          img.setAttribute("src", reader.result);
-        });
-        reader.readAsDataURL(choosedFile);
-      }
-    });
-  }
-
-  // carousels
-  $(".owl-carousel").owlCarousel({
+  $(".brands .owl-carousel").owlCarousel({
     center: true,
-    items:2,
-    loop:true,
-    margin:10,
+    items: 2,
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    navText: [
+      "<i class='fa-solid fa-chevron-right'></i>",
+      "<i class='fa-solid fa-chevron-left'></i>",
+    ],
+    rtl: dirAr,
     responsive: {
       0: {
-        items: 1.3,
+        items: 3,
       },
       600: {
-        items: 2.3,
+        items: 5,
+      },
+      992: {
+        items: 10,
+      },
+    },
+  });
+
+  $(".screens .owl-carousel").owlCarousel({
+    center: true,
+    items: 2,
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    navText: [
+      "<i class='fa-solid fa-chevron-right'></i>",
+      "<i class='fa-solid fa-chevron-left'></i>",
+    ],
+    rtl: dirAr,
+    responsive: {
+      0: {
+        items: 1.5,
+      },
+      600: {
+        items: 3,
       },
       992: {
         items: 5,
       },
     },
   });
-
-  // nise select
-  $("select").niceSelect();
 });
 
 
